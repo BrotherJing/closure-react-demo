cad2cc27724c5ec9960999da473b3f56b08d046e
Brotherjing <brotherjing@sjtu.edu.cn>
2018-11-04 15:18:04 +0800

    add closure component and react component

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,18 @@
 'use strict';
 goog.module('app');
+
+const notepad = goog.require('tutorial.notepad');
+
+const Timer = require('./react/timer');
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-class HelloWorld extends React.Component {
-    render() {
-        return React.createElement('div', null, 'Hello world!');
-    }
-}
+// react component
+ReactDOM.render(React.createElement(Timer, null), document.getElementById('app'));
 
-ReactDOM.render(React.createElement(HelloWorld, null), document.getElementById('app'));
+// closure library component
+var noteData = [
+    {'title': 'Note 1', 'content': 'Content of Note 1'},
+    {'title': 'Note 2', 'content': 'Content of Note 2'}];
+var noteListElement = document.getElementById('notes');
+var notes = tutorial.notepad.makeNotes(noteData, noteListElement);
---
+++ src/js/react/timer.js
@@ -0,0 +1,34 @@
+const React = require('react');
+
+class Timer extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { seconds: 0 };
+    }
+  
+    tick() {
+      this.setState(state => ({
+        seconds: state.seconds + 1
+      }));
+    }
+  
+    componentDidMount() {
+      this.interval = setInterval(() => this.tick(), 1000);
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.interval);
+    }
+  
+    render() {
+      return React.createElement(
+        "div",
+        null,
+        "Seconds: ",
+        this.state.seconds
+      );
+    }
+  }
+
+  module.exports = Timer;
+  
